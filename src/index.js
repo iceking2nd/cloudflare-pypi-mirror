699e839051c1e74cbbe10b05acf3c45df06639ff
Daniel Wu <wxc@wxccs.org>
2024-11-13 21:55:43 +0800

    first release

---
+++ src/index.js
@@ -0,0 +1,71 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+import _ from 'lodash-es'
+
+export default {
+	async fetch(request, env, ctx) {
+
+		const Pypi = "pypi.org";
+		const PyFiles = "files.pythonhosted.org";
+		const routes = {
+			["pypi." + env.CUSTOM_DOMAIN]: Pypi,
+			["py-files." + env.CUSTOM_DOMAIN]: PyFiles,
+		};
+
+		function routeByHosts(host){
+			if (host in routes) {
+				return routes[host];
+			}
+			if (env.MODE === "debug") {
+				return env.TARGET_UPSTREAM;
+			}
+			return "";
+		}
+
+		const url = new URL(request.url);
+		const upstream = routeByHosts(url.hostname);
+		if (upstream === "") {
+			return new Response(
+				JSON.stringify({
+					routes: routes,
+				}),
+				{
+					status: 404,
+				}
+			);
+		}
+
+		const newUrl = new URL("https://" + upstream + url.pathname);
+		const newReq = new Request(newUrl, {
+			method: request.method,
+			headers: request.headers,
+			redirect: "follow",
+		});
+		const resp = await fetch(newReq);
+		const newHeaders = new Headers();
+		newHeaders.set("content-type", resp.headers.get("content-type"));
+		if (upstream === Pypi) {
+			const body = await resp.text();
+			const newBody = _.replace(body, new RegExp(PyFiles,"g"),_.findKey(routes,function(o){
+				return o === PyFiles;
+			}));
+			newHeaders.set("content-length", String(newBody.length));
+			return new Response(newBody,{
+				status: resp.status,
+				headers: newHeaders
+			})
+		}
+		newHeaders.set("content-length", resp.headers.get("content-length"));
+		return new Response(resp.body,{
+			status: resp.status,
+			headers: newHeaders,
+		});
+	},
+};
